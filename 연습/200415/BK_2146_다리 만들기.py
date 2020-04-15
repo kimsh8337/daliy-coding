55c1f097c13c915e7da68eb7ff2f26dcda4cb234
김석환 <tokany@naver.com>
2020-04-15 22:34:07 +0900

    BK_2146 다리만들기

--- 연습/200415/BK_2146_다리 만들기.py
+++ 연습/200415/BK_2146_다리 만들기.py
@@ -5,30 +5,53 @@ from _collections import deque
 dr = [1,0,-1,0]
 dc = [0,1,0,-1]
 
+def bfs1(r,c,cnt):
+    q.append((r,c))
+    visited[r][c] = cnt
+    while q:
+        r,c = q.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0<=nr<n and 0<=nc<n:
+                if arr[nr][nc] == 1 and visited[nr][nc] == 0:
+                    visited[nr][nc] = cnt
+                    q.append((nr,nc))
+
+
+def bfs2(num):
+    while q:
+        r,c = q.popleft()
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0<=nr<n and 0<=nc<n:
+                if arr[nr][nc] == 1 and visited[nr][nc] != num:
+                    return visited2[r][c] - 1
+                if arr[nr][nc] == 0 and visited2[nr][nc] == 0:
+                    visited2[nr][nc] = visited2[r][c] + 1
+                    q.append((nr,nc))
+
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0]*n for _ in range(n)]
 ans = float('inf')
-res = 0
 q = deque()
+cnt = 1
 for i in range(n):
     for j in range(n):
-        if arr[i][j] == 0:
-            q.append((i,j,0))
+        if arr[i][j] == 1 and visited[i][j] == 0:
+            bfs1(i,j,cnt)
+            cnt += 1
 
-            while q:
-                r,c,cnt = q.popleft()
-                for k in range(4):
-                    nr = r + dr[k]
-                    nc = c + dc[k]
-                    if 0<=nr<n and 0<=nc<n and not visited[nr][nc]:
-                        if arr[nr][nc] == 1:
-                            visited[nr][nc] = 1
-                            q.append((nr,nc,0))
-                        else:
-                            visited[nr][nc] = 1
-                            q.append((nr,nc,cnt+1))
-                            res = cnt + 1
-                            # print(cnt)
-            ans = min(ans, res)
+for k in range(1,cnt):
+    q = deque()
+    visited2 = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == 1 and visited[i][j] == k:
+                q.append((i,j))
+                visited2[i][j] = 1
+    res = bfs2(k)
+    ans = min(ans,res)
 print(ans)
