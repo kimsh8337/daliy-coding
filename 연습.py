b11c0454feca3499023ed5b95e61a08d9bb2b435
김석환 <tokany@naver.com>
2020-04-28 22:44:16 +0900

    BK_7576_토마토

--- 연습.py
+++ 연습.py
@@ -1,23 +1,80 @@
 import sys
 sys.stdin = open('input.txt', 'r')
+from _collections import deque
 
-n = int(input())
-arr = []
-cnt = 0
-for _ in range(n):
-    arr.append(input())
+dr = [1,0,-1,0]
+dc = [0,1,0,-1]
 
-for i in range(n):
-    check = []
-    for j in range(len(arr[i])):
-        if arr[i][j] not in check:
-            check.append(arr[i][j])
-        else:
-            if arr[i][j] == arr[i][j-1]:
-                continue
-            else:
-                break
+def bfs(rr,cc):
+    global ans
+    q = deque()
+    q.append((rr, cc, 0))
+    visited[rr][cc] = 1
+
+    while q:
+        r, c, cnt = q.popleft()
+
+        for k in range(4):
+            nr = r + dr[k]
+            nc = c + dc[k]
+            if 0<=nr<n and 0<=nc<m:
+                if visited[nr][nc] == 1 or arr[nr][nc] == -1: continue
+                visited[nr][nc] = arr[nr][nc] = 1
+                q.append((nr,nc,cnt+1))
+                ans = cnt + 1
+    for i in arr:
+        if 0 in i:
+            return -1
     else:
-        cnt += 1
+        return ans
 
-print(cnt)
+m,n = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
+ans = 0
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1:
+            # bfs(i,j)
+            res = bfs(i,j)
+print(res)
+
+
+
+# from _collections import deque
+#
+# dr = [1,0,-1,0]
+# dc = [0,1,0,-1]
+#
+# def bfs():
+#     global ans
+#
+#     while q:
+#         r, c, cnt = q.popleft()
+#
+#         for k in range(4):
+#             nr = r + dr[k]
+#             nc = c + dc[k]
+#             if 0<=nr<n and 0<=nc<m:
+#                 if visited[nr][nc] == 1 or arr[nr][nc] == -1: continue
+#                 visited[nr][nc] = arr[nr][nc] = 1
+#                 q.append((nr,nc,cnt+1))
+#                 ans = cnt + 1
+#     for i in arr:
+#         if 0 in i:
+#             return -1
+#     else:
+#         return ans
+#
+# m,n = map(int, input().split())
+# arr = [list(map(int, input().split())) for _ in range(n)]
+# visited = [[0]*m for _ in range(n)]
+# ans = 0
+# q = deque()
+# for i in range(n):
+#     for j in range(m):
+#         if arr[i][j] == 1:
+#             q.append((i, j, 0))
+#             visited[i][j] = 1
+# print(bfs())
+#
---
+++ 연습/20.04/200428/BK_7576_토마토.py
@@ -0,0 +1,39 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+from _collections import deque
+
+dr = [1,0,-1,0]
+dc = [0,1,0,-1]
+
+def bfs():
+    global ans
+
+    while q:
+        r, c, cnt = q.popleft()
+
+        for k in range(4):
+            nr = r + dr[k]
+            nc = c + dc[k]
+            if 0<=nr<n and 0<=nc<m:
+                if visited[nr][nc] == 1 or arr[nr][nc] == -1: continue
+                visited[nr][nc] = arr[nr][nc] = 1
+                q.append((nr,nc,cnt+1))
+                ans = cnt + 1
+    for i in arr:
+        if 0 in i:
+            return -1
+    else:
+        return ans
+
+m,n = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
+ans = 0
+q = deque()
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 1:
+            q.append((i, j, 0))
+            visited[i][j] = 1
+print(bfs())
+
