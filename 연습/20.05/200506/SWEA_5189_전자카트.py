6271f5bfa1545d862556335080d8b1c80d05536a
김석환 <tokany@naver.com>
2020-05-06 17:45:17 +0900

    200506

--- 연습/20.05/200506/SWEA_5189_전자카트.py
+++ 연습/20.05/200506/SWEA_5189_전자카트.py
@@ -1,28 +1,29 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-def check(i,sum):
-    global min_sum
+def check(i):
+    global min_sum, sum
 
-    if min_sum < sum:
-        return
-
-    if i == n:
-        if min_sum > sum:
-            min_sum = sum
-        return
-
-    for j in range(n):
-        if visit[j]: continue
-        if not arr[i][j] : continue
-        visit[j] = 1
-        check(i+1,sum+arr[i][j])
-        visit[j] = 0
+    if sum < min_sum:
+        visit[i] = 1
+        if 0 not in visit:
+            sum += arr[i][0]
+            if sum < min_sum:
+                min_sum = sum
+            sum -= arr[i][0]
+        else:
+            for j in range(n):
+                if not visit[j]:
+                    sum += arr[i][j]
+                    check(j)
+                    sum -= arr[i][j]
+        visit[i] = 0
 
 for tc in range(1,1+int(input())):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
     visit = [0]*n
     min_sum = float('inf')
-    check(0,0)
+    sum = 0
+    check(0)
     print('#{} {}'.format(tc, min_sum))
