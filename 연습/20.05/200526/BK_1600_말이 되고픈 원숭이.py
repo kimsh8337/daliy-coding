1d0a7b3ffb83a10453c686cbad88370daba19925
김석환 <tokany@naver.com>
2020-05-29 12:47:39 +0900

    수정

--- 연습/20.05/200526/BK_1600_말이 되고픈 원숭이.py
+++ 연습/20.05/200526/BK_1600_말이 되고픈 원숭이.py
@@ -21,14 +21,14 @@ def bfs():
             for j in range(8):
                 nr = r + h_move[j][0]
                 nc = c + h_move[j][1]
-                if 0 <= nr < h and 0 <= nc < w and not arr[nr][nc]:
+                if 0 <= nr < h and 0 <= nc < w and not arr[nr][nc] and not visit[cnt+1][nr][nc]:
                     visit[cnt+1][nr][nc] = visit[cnt][r][c] + 1
                     Q.append((nr, nc, cnt + 1))
 
         for j in range(4):
             nr = r + m_move[j][0]
             nc = c + m_move[j][1]
-            if 0 <= nr < h and 0 <= nc < w and not arr[nr][nc]:
+            if 0 <= nr < h and 0 <= nc < w and not arr[nr][nc] and not visit[cnt][nr][nc]:
                 visit[cnt][nr][nc] = visit[cnt][r][c] + 1
                 Q.append((nr, nc, cnt))
     return -1
@@ -39,5 +39,6 @@ w, h = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(h)]
 visit = [[[0] * w for _ in range(h)] for _ in range(k+1)]
 print(bfs())
+
 # visit = [[[0] * (k+1) for _ in range(w)] for _ in range(h)]
 # print(visit)
