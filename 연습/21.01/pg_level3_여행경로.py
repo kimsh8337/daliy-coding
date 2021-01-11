5fc594f76faf1d92324bff1bbaf4ec2dd8478e35
김석환 <kimsh8337@gmail.com>
2021-01-11 17:52:21 +0900

    programmers_level3_여행경로

--- 연습/21.01/pg_level3_여행경로.py
+++ 연습/21.01/pg_level3_여행경로.py
@@ -1,5 +1,7 @@
 # tickets = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
-tickets = [["ICN","AAA"], ["ICN","BBB"], ["BBB","ICN"]]
+# tickets = [["ICN","AAA"], ["ICN","BBB"], ["BBB","ICN"]]
+# tickets = [["ICN", "A"], ["A", "B"], ["B", "A"], ["A", "ICN"], ["ICN", "A"]]
+tickets = [["ICN", "A"], ["ICN", "A"], ["A", "ICN"]]
 
 from collections import deque
 
@@ -30,11 +32,15 @@ def solution(tickets):
                 while Q:
                     check = []
                     st, ed = Q.popleft()
-                    path.append(ed)
+                    if st == path[-1]:
+                        path.append(ed)
+                    else:
+                        continue
 
                     for k in range(len(tickets)):
-                        if tickets[k][0] == st and tickets[k][1] == ed:
+                        if tickets[k][0] == st and tickets[k][1] == ed and visited[k] == 0:
                             visited[k] = 1
+                            temp = k
                             break
 
                     if not 0 in visited:
@@ -46,12 +52,15 @@ def solution(tickets):
                     if check:
                         check.sort()
                     else:
-                        break
+                        path.pop()
+                        visited[temp] = 0
+                        continue
 
-                    for j in range(len(tickets)):
-                        if check[0] == tickets[j][1] and tickets[j][0] == ed:
-                            Q.append((tickets[j][0], tickets[j][1]))
-                            break
+                    for k in range(len(check)):
+                        for j in range(len(tickets)):
+                            if check[k] == tickets[j][1] and tickets[j][0] == ed:
+                                Q.append((tickets[j][0], tickets[j][1]))
+
 
     return answer
 print(solution(tickets))
