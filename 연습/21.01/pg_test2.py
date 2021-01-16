935bd7d2f37657e1ab6fa10c3adaa56ebca08dd0
김석환 <kimsh8337@gmail.com>
2021-01-16 20:13:03 +0900

    pg_test2

--- 연습/21.01/pg_test2.py
+++ 연습/21.01/pg_test2.py
@@ -1,20 +1,39 @@
-n = 4
+# customer = [[1,1], [2,1], [3,1], [2,0], [2,1]]
+customer = [[2,1], [1,1], [3,1], [1,0], [1,1], [2,0], [2,1]]
+K = 1
 
-from itertools import combinations
-
-def solution(n):
+def solution(customer,K):
     answer = []
-    new = []
+    wait = []
     num = 0
 
-    for i in range(n):
-        new.append(i)
-    print(list(combinations(new,n-1)))
+    for i in range(len(customer)):
+        if num < K:
+            if customer[i][1] == 1:
+                answer.append(customer[i][0])
+                num += 1
+            elif customer[i][1] == 0:
+                if customer[i][0] in answer:
+                    answer.remove(customer[i][0])
+                    num -= 1
+                elif customer[i][0] in wait:
+                    wait.remove(customer[i][0])
 
-    # while num < n-1:
+        else:
+            if customer[i][1] == 1:
+                wait.append(customer[i][0])
+            elif customer[i][1] == 0:
+                if customer[i][0] in answer:
+                    answer.remove(customer[i][0])
+                    num -= 1
+                    if wait:
+                        answer.append(wait[0])
+                        num += 1
+                        wait.remove(wait[0])
+                elif customer[i][0] in wait:
+                    wait.remove(customer[i][0])
 
-
-
+    answer.sort()
     return answer
 
-print(solution(n))
+print(solution(customer,K))
