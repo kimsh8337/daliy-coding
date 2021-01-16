2b7366c2ed5b771f2d0be696e0b79fff0faef3d6
김석환 <kimsh8337@gmail.com>
2021-01-16 21:24:17 +0900

    pg_test3

--- 연습/21.01/pg_test2.py
+++ 연습/21.01/pg_test2.py
@@ -1,39 +1,67 @@
-# customer = [[1,1], [2,1], [3,1], [2,0], [2,1]]
-customer = [[2,1], [1,1], [3,1], [1,0], [1,1], [2,0], [2,1]]
-K = 1
+customer = [[1,1], [2,1], [3,1], [2,0], [2,1]]
+# customer = [[2,1], [1,1], [3,1], [1,0], [1,1], [2,0], [2,1]]
+K = 2
 
-def solution(customer,K):
-    answer = []
+# def solution(customer,K):
+#     answer = []
+#     wait = []
+#     num = 0
+#
+#     for i in range(len(customer)):
+#         if num < K:
+#             if customer[i][1] == 1:
+#                 answer.append(customer[i][0])
+#                 num += 1
+#             elif customer[i][1] == 0:
+#                 if customer[i][0] in answer:
+#                     answer.remove(customer[i][0])
+#                     num -= 1
+#                 elif customer[i][0] in wait:
+#                     wait.remove(customer[i][0])
+#
+#         else:
+#             if customer[i][1] == 1:
+#                 wait.append(customer[i][0])
+#             elif customer[i][1] == 0:
+#                 if customer[i][0] in answer:
+#                     answer.remove(customer[i][0])
+#                     num -= 1
+#                     if wait:
+#                         answer.append(wait[0])
+#                         num += 1
+#                         wait.remove(wait[0])
+#                 elif customer[i][0] in wait:
+#                     wait.remove(customer[i][0])
+#
+#     answer.sort()
+#     return answer
+#
+# print(solution(customer,K))
+
+
+
+
+def solution(customer, K):
+    rooms = []
     wait = []
-    num = 0
-
-    for i in range(len(customer)):
-        if num < K:
-            if customer[i][1] == 1:
-                answer.append(customer[i][0])
-                num += 1
-            elif customer[i][1] == 0:
-                if customer[i][0] in answer:
-                    answer.remove(customer[i][0])
-                    num -= 1
-                elif customer[i][0] in wait:
-                    wait.remove(customer[i][0])
-
+    for i in range(0, len(customer), 2):
+        num, check = customer[i], customer[i + 1]
+        if check == 1:
+            if K > 0:
+                K -= 1
+                rooms.append(num)
+            else:
+                wait.append(num)
         else:
-            if customer[i][1] == 1:
-                wait.append(customer[i][0])
-            elif customer[i][1] == 0:
-                if customer[i][0] in answer:
-                    answer.remove(customer[i][0])
-                    num -= 1
-                    if wait:
-                        answer.append(wait[0])
-                        num += 1
-                        wait.remove(wait[0])
-                elif customer[i][0] in wait:
-                    wait.remove(customer[i][0])
-
-    answer.sort()
-    return answer
+            if num in wait:
+                wait.remove(num)
+            else:
+                rooms.remove(num)
+                if len(wait):
+                    n_num = wait.pop(0)
+                    rooms.append(n_num)
+                else:
+                    K += 1
+    return sorted(rooms)
 
 print(solution(customer,K))
---
+++ 연습/21.01/pg_test3.py
@@ -0,0 +1,42 @@
+board = [[1,1,4,3], [3,2,1,4], [3,1,4,2], [2,1,3,3]]
+# board = [[1,2,1,2], [3,4,3,4], [1,2,1,2], [3,4,3,4]]
+
+dr = [1,0,-1,0]
+dc = [0,1,0,-1]
+
+def solution(board):
+    def check(r,c):
+        nonlocal answer
+
+        for a in range(4):
+            nr = r + dr[a]
+            nc = c + dc[a]
+            if 0<=nr<len(board) and 0<=nc<len(board):
+                board[r][c], board[nr][nc] = board[nr][nc], board[r][c]
+                if 0<=r+2<len(board) and board[r][c] == board[r+1][c] and board[r][c] == board[r+2][c]:
+                    answer += 1
+                elif 0<=r-2<len(board) and board[r][c] == board[r-1][c] and board[r][c] == board[r-2][c] :
+                    answer += 1
+                elif 0<=c+2<len(board) and board[r][c] == board[r][c+1] and board[r][c] == board[r][c+2]:
+                    answer += 1
+                elif 0<=c-2<len(board) and board[r][c] == board[r][c-1] and board[r][c] == board[r][c-2]:
+                    answer += 1
+                elif 0<=r-1<len(board) and 0<=r+1<len(board) and board[r][c] == board[r-1][c] and board[r][c] == board[r+1][c]:
+                    answer += 1
+                elif 0<=c-1<len(board) and 0<=c+1<len(board) and board[r][c] == board[r][c-1] and board[r][c] == board[r][c+1]:
+                    answer += 1
+                board[r][c], board[nr][nc] = board[nr][nc], board[r][c]
+        return
+
+    answer = 0
+
+    for i in range(len(board)):
+        for j in range(len(board)):
+            # visited = [[0] * len(board) for _ in range(len(board))]
+            check(i,j)
+
+    if answer == 0:
+        answer = -1
+    return answer
+
+print(solution(board))
