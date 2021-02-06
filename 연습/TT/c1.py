2855cf4fdd559582c03da9a5bd4e55771828222f
김석환 <kimsh8337@gmail.com>
2021-02-06 19:10:41 +0900

    programmers_level3_자물쇠와 열쇠

---
+++ 연습/TT/c1.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+
+n = int(input())
+
+for i in range(n):
+    for j in range(n+i):
+        if j < n-i-1:
+            print(' ',end='')
+        else:
+            print('*',end='')
+    print()
