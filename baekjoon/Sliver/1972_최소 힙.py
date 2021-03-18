46b1314af9b448a45b0a169b418d7e86fe2961ff
김석환 <kimsh8337@gmail.com>
2021-03-18 22:27:46 +0900

    baekjoon_1927_최소힙

---
+++ baekjoon/Sliver/1972_최소 힙.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+import heapq
+
+heap = []
+
+for _ in range(int(input())):
+    x = int(input())
+
+    if x == 0:
+        if not heap:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap,x)
