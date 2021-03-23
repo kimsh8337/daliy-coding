ee8c4c60cf040d4e4f4c35387e0262e88b3dccbd
김석환 <kimsh8337@gmail.com>
2021-03-23 23:06:37 +0900

    baekjoon_9095_1,2,3더하기

---
+++ baekjoon/Sliver/9095_1,2,3 더하기.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
+
+dp = [0,1,2,4]
+for _ in range(4,14):
+    dp.append(sum(dp[-3:]))
+for _ in range(int(input())):
+    print(dp[int(input())])
